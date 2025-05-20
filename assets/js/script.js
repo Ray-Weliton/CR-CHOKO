b9f335fa2db3efb47c5cd89cdb69320a68497aa3
Ray-Weliton <f.weliton@aluno.ifsp.edu.br>
2025-05-20 16:43:28 -0300

    sessão sobre

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,23 @@ imagens.forEach((img) => {
 lightbox.addEventListener('click', () => {
     lightbox.classList.remove('show');
 })
+
+const openLoginBtn = document.getElementById('openLogin')
+const containerLogin = document.querySelector('.container-login')
+
+openLoginBtn.addEventListener('click', () => {
+    containerLogin.classList.add('active')
+})
+
+// Fechar ao clicar fora do formulário ou pressionar ESC
+containerLogin.addEventListener('click', (e) => {
+    if(e.target === containerLogin) {
+        containerLogin.classList.remove('active')
+    }
+})
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        containerLogin.classList.remove('active')
+    }
+})
